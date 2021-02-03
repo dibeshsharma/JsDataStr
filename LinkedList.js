2818267f55189c2bc7a9c6482d2d6e8400c26398
dibeshsharma <dibeshsharma@gmail.com>
2021-02-03 14:04:22 +1100

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -18,8 +18,12 @@ class LinkedList {
   
     // functions to be implemented 
     
-	// adds an element at the end 
-	// of list 
+	// adds an element at the end of list	
+	//In order to add an element at the end of the list we consider three conditions as follows:
+	//if the index is zero we add an element at the front of the list and make it head
+	//If the index is the last position of the list we append the element at the end of the list
+	//if the index is inbetween 0 or size – 1 we iterate over to the index and add an element at that index
+	
 	add(element) 
 	{ 
 	    // creates a new node 
